e9980d3d6e5d430f4689efa346cbd2c34a99abad
Stanislas MEZUREUX <stanislas.mezureux@telecomnancy.eu>
2024-02-14 16:11:29 +0100

    [FIX] update new home page

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard/videos';
+    public const HOME = '/dashboard/quizz';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
